989202290d265e61c5349e0c5019b39409213017
Joseph-Schtein <yossiand10@gmail.com>
2020-03-23 15:24:30 +0200

    add more tests to the file

--- Test.cpp
+++ Test.cpp
@@ -12,7 +12,6 @@ TEST_CASE("Test replacement of f and b and p, and lower & upper case") {
     CHECK(find(text, "pataL") == string("fatal"));
     CHECK(find(text, "FailUre") == string("failure"));
     CHECK(find(text, "CouNts") == string("counts"));
-    
 }
 
 
@@ -49,11 +48,11 @@ TEST_CASE("Test replacement of s and z, and lower & upper case") {
 
 
 TEST_CASE("Test replacement of d and t, and lower & upper case") {
-    string text = "this report by its very length defends itself against the Tisk if being read";
+    string text = "this report by its very length defends itself against the Disk if being read";
     CHECK(find(text, "Tefents") == string("defends"));
     CHECK(find(text, "reporD") == string("report"));
     CHECK(find(text, "reat") == string("read"));
-    CHECK(find(text, "risk") == string("Tisk"));
+    CHECK(find(text, "tisk") == string("Disk"));
 }
 
 
@@ -80,76 +79,71 @@ TEST_CASE("Test replacement of i and y, and lower & upper case") {
 
 
 
-TEST_CASE("Test replacement of p and b") {
-    string text = "It has been said that democracy is the worst form of government except all the others that have been tried";
-    CHECK(find(text, "happy") == string("happy"));
-    CHECK(find(text, "habby") == string("happy"));
-    CHECK(find(text, "hapby") == string("happy"));
-    CHECK(find(text, "habpy") == string("happy"));
-    /* Add more checks here */
+TEST_CASE("Test replace muiltply letters, and change cases") {
+    string text = "It has been said that democracy is the WorSt form of joWernmenD except all the others that have been tried";
+    CHECK(find(text, "government") == string("joWernmenD"));
+    CHECK(find(text, "temoKraqi") == string("democracy"));
+    CHECK(find(text, "worst") == string("WorSt"));
+    CHECK(find(text, "Utherz") == string("others"));
+    
 }
 
 
 
-TEST_CASE("Test replacement of p and b") {
-    string text = "history is written by the victors";
-    CHECK(find(text, "happy") == string("happy"));
-    CHECK(find(text, "habby") == string("happy"));
-    CHECK(find(text, "hapby") == string("happy"));
-    CHECK(find(text, "habpy") == string("happy"));
-    /* Add more checks here */
-}
-
-
-
-
-TEST_CASE("Test replacement of p and b") {
-    string text = "the enemy of my enemy is my friend";
-    CHECK(find(text, "happy") == string("happy"));
-    CHECK(find(text, "habby") == string("happy"));
-    CHECK(find(text, "hapby") == string("happy"));
-    CHECK(find(text, "habpy") == string("happy"));
-    /* Add more checks here */
+TEST_CASE("Test replace muiltply letters, and change cases") {
+    string text = "history is vrYdden by the victors";
+    CHECK(find(text, "hYstorI") == string("history"));
+    CHECK(find(text, "written") == string("vrYdden"));
+    CHECK(find(text, "by") == string("by"));
+    CHECK(find(text, "wyctorz") == string("victors"));
 }
 
 
 
 
-TEST_CASE("Test replacement of p and b") {
-    string text = "If a man does his best what else is there";
-    CHECK(find(text, "happy") == string("happy"));
-    CHECK(find(text, "habby") == string("happy"));
-    CHECK(find(text, "hapby") == string("happy"));
-    CHECK(find(text, "habpy") == string("happy"));
-    /* Add more checks here */
+TEST_CASE("Test replace muiltply letters, and change cases") {
+    string text = "Dhe enemy of my enemy is my prient";
+    CHECK(find(text, "EnemY") == string("enemy"));
+    CHECK(find(text, "friend") == string("prient"));
+    CHECK(find(text, "Mi") == string("my"));
+    CHECK(find(text, "the") == string("Dhe"));
+}
+
+
+
+
+TEST_CASE("Test replace muiltply letters, and change cases") {
+    string text = "IB a MAN does his best what else is there";
+    CHECK(find(text, "There") == string("there"));
+    CHECK(find(text, "Vhad") == string("what"));
+    CHECK(find(text, "man") == string("MAN"));
+    CHECK(find(text, "If") == string("IB"));
 }
 
 
 
 
 
-TEST_CASE("Test replacement of p and b") {
-    string text = "Courage is fear hoding on a minute longer";
-    CHECK(find(text, "happy") == string("happy"));
-    CHECK(find(text, "habby") == string("happy"));
-    CHECK(find(text, "hapby") == string("happy"));
-    CHECK(find(text, "habpy") == string("happy"));
-    /* Add more checks here */
+TEST_CASE("Test replace muiltply letters, and change cases") {
+    string text = "Courage is Bear hoding on a minute LunJer";
+    CHECK(find(text, "fear") == string("Bear"));
+    CHECK(find(text, "mYnude") == string("minute"));
+    CHECK(find(text, "longer") == string("LunJer"));
+    CHECK(find(text, "A") == string("a"));
 }
 
 
-TEST_CASE("Test replacement of p and b") {
-    string text = "If everyone is thinking alike then sombody isn't thinking ";
-    CHECK(find(text, "happy") == string("happy"));
-    CHECK(find(text, "habby") == string("happy"));
-    CHECK(find(text, "hapby") == string("happy"));
-    CHECK(find(text, "habpy") == string("happy"));
-    /* Add more checks here */
+TEST_CASE("Test replace muiltply letters, and change cases") {
+    string text = "If ewerIune is thinking ALIKE then sombody is not thinking ";
+    CHECK(find(text, "everyone") == string("ewerIune"));
+    CHECK(find(text, "ZumbUdi") == string("sombody"));
+    CHECK(find(text, "thYncinG") == string("thinking"));
+    CHECK(find(text, "alike") == string("ALIKE"));
 }
 
 
 
-TEST_CASE("Test replacement of p and b") {
+TEST_CASE("Test replace muiltply letters, and change cases") {
     string text = "Battle is an orgy of disorder";
     CHECK(find(text, "happy") == string("happy"));
     CHECK(find(text, "habby") == string("happy"));
@@ -160,7 +154,7 @@ TEST_CASE("Test replacement of p and b") {
 
 
 
-TEST_CASE("Test replacement of p and b") {
+TEST_CASE("Test replace muiltply letters, and change cases") {
     string text = "Success is how high you bounce when you hit bottom";
     CHECK(find(text, "happy") == string("happy"));
     CHECK(find(text, "habby") == string("happy"));
@@ -171,7 +165,7 @@ TEST_CASE("Test replacement of p and b") {
 
 
 
-TEST_CASE("Test replacement of p and b") {
+TEST_CASE("Test replace muiltply letters, and change cases") {
     string text = "Better to fight for something than live for nothing";
     CHECK(find(text, "happy") == string("happy"));
     CHECK(find(text, "habby") == string("happy"));
@@ -183,7 +177,7 @@ TEST_CASE("Test replacement of p and b") {
 
 
 
-TEST_CASE("Test replacement of p and b") {
+TEST_CASE("Test replace muiltply letters, and change cases") {
     string text = "It is not that I am so smart it is just that I stay with problems longer ";
     CHECK(find(text, "happy") == string("happy"));
     CHECK(find(text, "habby") == string("happy"));
@@ -194,7 +188,7 @@ TEST_CASE("Test replacement of p and b") {
 
 
 
-TEST_CASE("Test replacement of p and b") {
+TEST_CASE("Test replace muiltply letters, and change cases") {
     string text = "Pure mathematics is in its way the poetry of logical ideas";
     CHECK(find(text, "happy") == string("happy"));
     CHECK(find(text, "habby") == string("happy"));
@@ -205,7 +199,7 @@ TEST_CASE("Test replacement of p and b") {
 
 
 
-TEST_CASE("Test replacement of p and b") {
+TEST_CASE("Test replace muiltply letters, and change cases") {
     string text = "Love is a better teacher than duty";
     CHECK(find(text, "happy") == string("happy"));
     CHECK(find(text, "habby") == string("happy"));
@@ -216,7 +210,7 @@ TEST_CASE("Test replacement of p and b") {
 
 
 
-TEST_CASE("Test replacement of p and b") {
+TEST_CASE("Test replace muiltply letters, and change cases") {
     string text = "When the solution is simple God is answering";
     CHECK(find(text, "happy") == string("happy"));
     CHECK(find(text, "habby") == string("happy"));
@@ -227,7 +221,7 @@ TEST_CASE("Test replacement of p and b") {
 
 
 
-TEST_CASE("Test replacement of p and b") {
+TEST_CASE("Test replace muiltply letters, and change cases") {
     string text = "God always takes the simpelst way";
     CHECK(find(text, "happy") == string("happy"));
     CHECK(find(text, "habby") == string("happy"));
@@ -239,7 +233,7 @@ TEST_CASE("Test replacement of p and b") {
 
 
 
-TEST_CASE("Test replacement of p and b") {
+TEST_CASE("Test replace muiltply letters, and change cases") {
     string text = "The faster you go the shorter you are";
     CHECK(find(text, "happy") == string("happy"));
     CHECK(find(text, "habby") == string("happy"));
@@ -251,7 +245,7 @@ TEST_CASE("Test replacement of p and b") {
 
 
 
-TEST_CASE("Test replacement of p and b") {
+TEST_CASE("Test replace muiltply letters, and change cases") {
     string text = "Information is not knowledge";
     CHECK(find(text, "happy") == string("happy"));
     CHECK(find(text, "habby") == string("happy"));
@@ -262,7 +256,7 @@ TEST_CASE("Test replacement of p and b") {
 
 
 
-TEST_CASE("Test replacement of p and b") {
+TEST_CASE("Test replace muiltply letters, and change cases") {
     string text = "Coincidence is Gods way of remaining anonymous";
     CHECK(find(text, "happy") == string("happy"));
     CHECK(find(text, "habby") == string("happy"));
@@ -274,7 +268,7 @@ TEST_CASE("Test replacement of p and b") {
 
 
 
-TEST_CASE("Test replacement of p and b") {
+TEST_CASE("Test replace muiltply letters, and change cases") {
     string text = "live laught love";
     CHECK(find(text, "happy") == string("happy"));
     CHECK(find(text, "habby") == string("happy"));
